cbdd893ce21bd1c20b75db1387ef92ea352cd92b
nGhart <naaghart@gmail.com>
2023-08-18 13:47:56 +0000

    reviews

--- src/Components/Review.js
+++ src/Components/Review.js
@@ -1,69 +1,11 @@
 import React from 'react';
+import ReviewItems from '../Components/ReviewItems';
 
 const Review = () => {
   return (
-    <div className="container reviewSection" style={{paddingBottom: '160px'}}>
-      <div className="title text-center">
-        <h1>Reviews</h1>
-      </div>
-      <div className="row reviewPlusControls">
-        <div className="col-xs-1 col-sm-2">
-          <div className="prev">
-            <button
-              className="leftB"
-              type="button"
-              data-bs-target="#carouselExampleControls"
-              data-bs-slide="prev"
-            >
-              <span
-                className="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-            </button>
-          </div>
-        </div>
-
-        <div className="col-xs-10 col-sm-8">
-          <div className="card text-center reviewContainer">
-            <img
-              src=""
-              className="reviewImage mx-auto d-block"
-              id="image"
-              alt="..."
-              height="200px"
-              width="30%"
-            />
-            <div className="card-body">
-              <h5 className="card-title">
-                "<span id="highlight"></span>"
-              </h5>
-              <p className="card-text" id="reviewText"></p>
-              <p className="card-text" id="credit"></p>
-              <h6
-                className="card-subtitle mb-2 text-muted"
-                id="customerName"
-              ></h6>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-xs-1 col-sm-2">
-          <div className="next">
-            <button
-              className="rightB"
-              type="button"
-              data-bs-target="#carouselExampleControls"
-              data-bs-slide="next"
-            >
-              <span
-                className="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <ReviewItems />
+    </>
   );
 };
 
--- src/Components/reviewData.js
+++ src/Components/reviewData.js
@@ -1,34 +1,52 @@
-const reviews = [
+import matheus from '../images/matheus.jpg';
+import ayo from '../images/ayo.jpg';
+import ian from '../images/ian.jpg';
+import vince from '../images/vince.jpg';
+
+export const reviews = [
   {
-    img: './images/matheus.jpg',
+    img: matheus,
     highlight: '... porttitor ...',
     review: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.`,
-    credit: `Photo by <a href="https://unsplash.com/@matheusferrero?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Matheus Ferrero</a> on <a href="https://unsplash.com/images/people?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-        `,
+    imageLink:
+      'https://unsplash.com/@matheusferrero?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+    artist: 'Matheus Ferrero',
+    siteLink:
+      'https://unsplash.com/images/people?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
     name: 'Ama',
   },
   {
-    img: './images/ayo.jpg',
+    img: ayo,
     highlight: 'Pellentesque habitant morbi tristique ...',
-    review: ` Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa.`,
-    credit: `Photo by <a href="https://unsplash.com/@armedshutter?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ayo Ogunseinde</a> on <a href="https://unsplash.com/images/people?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-        `,
+    review:
+      'Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa.',
+    imageLink:
+      'https://unsplash.com/@armedshutter?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+    artist: 'Ayo Ogunseinde',
+    siteLink:
+      'https://unsplash.com/images/people?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
     name: 'Abena',
   },
   {
-    img: './images/ian.jpg',
+    img: ian,
     highlight: '... scelerisque at, vulputate vitae ...',
     review: ` Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa.`,
-    credit: `Photo by <a href="https://unsplash.com/@sadswim?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">ian dooley</a> on <a href="https://unsplash.com/images/people?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-        `,
-    name: 'Abena',
+    imageLink:
+      'https://unsplash.com/@sadswim?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+    artist: 'Ian dooley',
+    siteLink:
+      'https://unsplash.com/images/people?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+    name: 'Kofi',
   },
   {
-    img: './images/vince.jpg',
+    img: vince,
     highlight: '... semvenenatis eleifend.',
     review: `Mauris eget neque at semvenenatis eleifend. Ut nonummy. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa.`,
-    credit: `Photo by <a href="https://unsplash.com/@vincefleming?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Vince Fleming</a> on <a href="https://unsplash.com/images/people?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-        `,
-    name: 'Akua',
+    imageLink:
+      'https://unsplash.com/@vincefleming?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+    artist: 'Vince Fleming',
+    siteLink:
+      'https://unsplash.com/images/people?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+    name: 'Yaw',
   },
 ];
